dabda32cbe29caa27f1bb597d964118ba7ba12c5
Paras Jain <paras7403@gmail.com>
2024-04-19 18:37:38 +0530

    first commit

---
+++ src/components/Layouts/Navbar.js
@@ -0,0 +1,39 @@
+import { IconButton } from '@chakra-ui/button';
+import {
+    Stack,
+    Flex,
+    Heading,
+    Spacer,
+    HStack,
+    Text,
+    Button,
+} from "@chakra-ui/react";
+import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
+
+const Navbar = () => {
+    return (
+        <Stack
+            p={5}
+            bg={'gray.50'}
+            as='header'
+        >
+            <Flex
+                w='full'
+                alignItems={'center'}
+            >
+                <Heading as='h3' ml={{ base: 0, sm: 8 }} size='lg' fontWeight={'thin'} color='purple.500' style={{ fontFamily: "Pacifico" }}>ResumeGen</Heading>
+                <Spacer></Spacer>
+                <HStack spacing={10} mr={{ base: 0, sm: 8 }} as='nav' style={{ fontFamily: 'Poppins' }}>
+                    <Text as='a' href='#' fontSize='lg'>About</Text>
+                    <Text as='a' href='#footer' fontSize='lg'>Contact</Text>
+                    {/* <Text as='a' href='#' fontSize='lg'>About</Text> */}
+                    {/* <Button href={'#builder'} colorScheme={'purple'} fontWeight={'medium'} >Templates</Button> */}
+                    <Button as="a" href="#builder" colorScheme="purple" fontWeight="medium">Templates</Button>
+                </HStack>
+            </Flex>
+
+        </Stack>
+    )
+}
+
+export default Navbar
